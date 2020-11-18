657b05bc3100eb40f732ed263fa17ae7e6efb73a
prajjwal1 <prajjwalin@protonmail.com>
2020-11-18 18:06:19 +0530

    added user authentication

--- node-cheese-cake/app.js
+++ node-cheese-cake/app.js
@@ -4,17 +4,62 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars')
+const MongoClient = require('mongodb').MongoClient;
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const session = require('express-session');
+const hbs = require('hbs');
+const flash = require('connect-flash');
+
+const authUtils = require('./utils/auth');
 
 var indexRouter = require('./routes/index');
-var signinRouter = require('./routes/signin');
+var usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
+MongoClient.connect('mongodb://localhost', (err, client) => {
+  if (err) {
+    throw err;
+  }
+
+  const db = client.db('cheese-users');
+  const users = db.collection('users');
+  app.locals.users = users;
+});
+
+passport.use(new Strategy(
+  (username, password, done) => {
+    app.locals.users.findOne({ username }, (err, user) => {
+      if (err){
+        return done(err);
+      }
+
+      if (!user){
+        return done(null, false);
+      }
+
+      if (user.password != authUtils.hashPassword(password)) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  } 
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+})
+
+passport.deserializeUser((id, done) => {
+  done(null, {id});
+})
+
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,21 +67,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/signin', signinRouter);
+app.use(session({
+  secret: 'session secret',
+  resave: false,
+  saveUninitialized: false,
+}));
 
-// catch 404 and forward to error handler
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.loggedIn = req.isAuthenticated();
+  next();
+});
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.use('/auth', authRouter);
+
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- node-cheese-cake/user-profiles/routes/index.js
+++ node-cheese-cake/user-profiles/routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Cheese Cake Shop' });
+  res.render('index', { title: 'Express' });
 });
-
-
 
 module.exports = router;
